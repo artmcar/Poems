a22600f3f95f19b6a0e4a299bc8c1794f159c3d5
Artur <makaryan.a.a@edu.mirea.ru>
2025-05-28 10:10:12 +0300

    Removed the ability to change the screen orientation

--- Poems/app/src/main/java/com/pirmp/poems/db/assetspoems/AssetsRepository.kt
+++ Poems/app/src/main/java/com/pirmp/poems/db/assetspoems/AssetsRepository.kt
@@ -2,7 +2,6 @@ package com.pirmp.poems.db.assetspoems
 
 import androidx.lifecycle.LiveData
 import com.pirmp.poems.db.AssetsPoemDao
-import com.pirmp.poems.db.userpoems.DbFields
 
 class AssetsRepository(private val assetsDao: AssetsPoemDao) {
     val getAllPoems : LiveData<List<AssetsDbFields>> = assetsDao.getAllPoems()
